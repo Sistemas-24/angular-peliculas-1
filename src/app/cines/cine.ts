a593b6deb8c0b88b149b793582ff9a0b0dea4e95
Sistemas-24 <sistemas24@avanceytec.com>
2024-12-12 14:32:09 -0600

    lmaomap

---
+++ src/app/cines/cine.ts
@@ -0,0 +1,14 @@
+export interface CineDTO{
+    id: number;
+    nombre: string;
+    latitud: number;
+    longitud: number;
+}
+ 
+export interface CineAgregarDTO {
+    nombre: string;
+   
+    latitud: number;
+    longitud: number;
+ 
+}
--- src/app/cines/editar-cine/editar-cine.component.ts
+++ src/app/cines/editar-cine/editar-cine.component.ts
@@ -1,12 +1,24 @@
 import { Component, Input, numberAttribute } from '@angular/core';
+import { CineAgregarDTO, CineDTO } from '../cine';
+import { FormularioCinesComponent } from "../formulario-cines/formulario-cines.component";
 
 @Component({
   selector: 'app-editar-cine',
-  imports: [],
+  imports: [FormularioCinesComponent],
   templateUrl: './editar-cine.component.html',
   styleUrl: './editar-cine.component.css'
 })
 export class EditarCineComponent {
   @Input({transform: numberAttribute})
   id!: number;
+
+  cine: CineDTO = {
+    id: 1, nombre: 'Acropolis',
+    latitud: 0,
+    longitud: 0
+  }
+
+  guardarCambios(cine: CineAgregarDTO){
+    console.log('editar cine', cine);
+  }
 }
